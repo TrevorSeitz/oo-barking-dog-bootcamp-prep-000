f63cf6bf0f308077f842d107c32e86c0525beb43
Trevor Seitz <trevinla@gmail.com>
2018-03-06 22:51:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,7 @@
 # Your code goes here!
 class Dog
-  def initialize(name)
-    @name = name
-  end
-
-  def name=(name)
-    @name = name
+  def name=(dog_name)
+    @name = dog_name
   end
 
   def name
